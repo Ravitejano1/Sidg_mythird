ee2e57bc8af84f4493ee67eaa351d2d7a9e62f00
Ravitejano1 <traviteja1000@gmail.com>
2022-10-17 16:37:06 +0530

    Updated by ravi

--- script5.sh
+++ script5.sh
@@ -1,5 +1,4 @@
 Hi this is Raviteja
 Hello this is bush
 Jenkins is a continuous Integration tool
-jenkins is an iopen source tool[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[3~[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-
+jenkins is an iopen source tool
